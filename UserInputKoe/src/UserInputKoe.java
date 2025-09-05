68b717444b46b542d9108fda6b44153c370ff865
Jogis <ronipihajoki@proton.me>
2025-09-05 22:56:40 +0300

    teht3
    teht3

--- UserInputKoe/src/UserInputKoe.java
+++ UserInputKoe/src/UserInputKoe.java
@@ -9,25 +9,26 @@ public class UserInputKoe {
         String nimi = "";
             System.out.println("What is your name?");
                 nimi = in.nextLine();
-        if (nimi.equals ("")){
-            System.out.println("Error");
-        }
-        else{
-            System.out.println("Your name is " + nimi + ".");
-            }
+        int ika = 0;
+            System.out.println("What is your age?");
+                ika = Integer.parseInt(in.nextLine());
+            System.out.println("Your name is " + nimi + " and you are " + ika + "years old.");
+        
     }
 }
 
 
 
-// At first print:
-// What is your name?
-// After user has typed the name, assign the input to a variable and print: Your name is ..... 
-// If the user just presses enter and won't write anything, print "Error".
-// The output in the console should be precisely, if the user types "Justin":
+// First ask the name of the user and then the age of the user. Then print:
+// Your name is Justin and you are 45 years old.
+// The output in the console should be precisely, if the user types "Justin" and "45":
 // What is your name?
 // Justin
-// Your name is Justin.
-// The output should be precisely, if the user won't type anything and just presses enter:
+// How old are you?
+// 45
+// Your name is Justin and you are 45 years old.
+// Notice! When CodeRunner checks the code, it doesn't print the user input. It should be like above,
+    // but in Test Cases output looks like (if the input was Justin and 45):
 // What is your name?
-// Error
+// How old are you?
+// Your name is Justin and you are 45 years old.
