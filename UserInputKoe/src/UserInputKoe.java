02e546dd0671653a6258109834a8e9d2a10e67c1
Jogis <ronipihajoki@proton.me>
2025-09-05 22:49:01 +0300

    Teht 2
    teht 2

--- UserInputKoe/src/UserInputKoe.java
+++ UserInputKoe/src/UserInputKoe.java
@@ -9,18 +9,25 @@ public class UserInputKoe {
         String nimi = "";
             System.out.println("What is your name?");
                 nimi = in.nextLine();
+        if (nimi.equals ("")){
+            System.out.println("Error");
+        }
+        else{
             System.out.println("Your name is " + nimi + ".");
-
+            }
     }
 }
 
 
 
-// Ask the name of the user and print it. More precisely:
 // At first print:
 // What is your name?
-// After the user has typed the name, assign the input to a variable and print: Your name is ..... 
-// The output in console should be precisely, if the user types "Justin":
+// After user has typed the name, assign the input to a variable and print: Your name is ..... 
+// If the user just presses enter and won't write anything, print "Error".
+// The output in the console should be precisely, if the user types "Justin":
 // What is your name?
 // Justin
 // Your name is Justin.
+// The output should be precisely, if the user won't type anything and just presses enter:
+// What is your name?
+// Error
